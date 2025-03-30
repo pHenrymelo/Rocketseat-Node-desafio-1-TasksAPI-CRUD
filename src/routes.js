903a1f0be4e6be22551142aa3794f0824c120d5f
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-03-30 15:05:29 -0300

    feat: add router utils integration

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,41 @@
+import { buildRoutePath } from './utils/build-route-path.js'
+import { Database } from './database/database.js'
+import { randomUUID } from 'node:crypto'
+
+const database = new Database()
 
 export const routes = [
     {
         method: "GET",
-        path: "/tasks",
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
             return res.end('listar tasks')
         }
     },
     {
         method: "POST",
-        path: "/tasks",
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
             return res.end('criar task')
         }
     },
     {
         method: "DELETE",
-        path: "/tasks/:id",
+        path: buildRoutePath('/tasks/:id'),
         handler: (req, res) => {
             return res.end('deletar task')
         }
     },
     {
         method: "PUT",
-        path: "/tasks/:id",
+        path: buildRoutePath('/tasks/:id'),
         handler: (req, res) => {
             return res.end('editar task')   
         }
     },
     {
         method: "PATCH",
-        path: "/tasks/:id/complete",
+        path: buildRoutePath('/tasks/:id/complete'),
         handler: (req, res) => {
             return res.end('marcar task como concluída')
         }

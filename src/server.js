9a14b32955000a49339ab346e140e306ad761b4e
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-03-30 14:45:05 -0300

    feat: add json middleware setup

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import http from 'node:http'
 import { routes } from './routes.js'
+import { json } from './middlewares/json.js'
 
-const algumaCoisa = "algo"
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
     const { method, url } = req
+
+    await json(req, res)
 
     const route = routes.find(route => {
         return route.method === method && route.path === url
